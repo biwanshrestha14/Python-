b339325add5cef4724c376119e9861db2af984b7
biwanshrestha14 <biwanshrestha77@gmail.com>
2025-02-19 10:42:20 +0545

    Fixed push issue

---
+++ first.py
@@ -0,0 +1,59 @@
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+        print("Five is greater than two!") 
+
+        x = 5
+y = "Hello, World!"
+#this is a comment
+print("Hello, World!") #This is a comment
+#print("Hello, World!")
+print("Cheers, Mate!")
+"""biwan shrestha"""
+a = int(5)
+# a = str("Biwan")
+# a= float(3)
+print(a)
+a,b,c= "Biwan","nikhil","kevin"
+
+print(type(a))
+fruits=['banana','apple',"orange"]
+a,b,c= fruits
+print(a)
+print(b)
+print(c)
+import random
+print(random.randrange(1,100))
+for x in "biwan shrestha":
+  print(x)
+
+
+
+
+let = "there is a different world inside me"
+print("different" in let)
+if "biwan" not  in let:
+    print("there is different present int he let")
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict["colors"])
+print(type(thisdict))
+
+thatdist = dict(name="biwan",age=30)
+print(thatdist)
+
+car={
+    "name":"audi",
+    "model":"supra",
+
+}
+#before modifying
+# print(car)
+#after modifying
+car["color"]="red"
+print(car.items())
